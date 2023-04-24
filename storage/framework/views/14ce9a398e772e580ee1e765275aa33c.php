0029bd8f11ccb78e28fd0190a7e4efeb3e46574e
Kokilan <kokilan.92@gmail.com>
2023-04-24 20:27:54 +0800

    Add files via upload

---
+++ storage/framework/views/14ce9a398e772e580ee1e765275aa33c.php
@@ -0,0 +1,89 @@
+<?php $__env->startSection('title', 'Dashboard'); ?>
+
+<?php $__env->startSection('content_header'); ?>
+    <h1>Dashboard</h1>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('content'); ?>
+    
+
+    <!-- Main content -->
+    
+        <div class="container-fluid">
+          <!-- Small boxes (Stat box) -->
+          <div class="row">
+            <div class="col-lg-3 col-6">
+              <!-- small box -->
+              <div class="small-box bg-info">
+                <div class="inner">
+                  <h3>
+                    <?php echo e($articles); ?>
+
+                  </h3>
+
+                  <p>Articles</p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-bag"></i>
+                </div>
+                <a href="/article" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-lg-3 col-6">
+              <!-- small box -->
+              <div class="small-box bg-success">
+                <div class="inner">
+                  <h3><?php echo e($interest_rate->current_interest_rate); ?><sup style="font-size: 20px">%</sup></h3>
+
+                  <p>Current Interest Rate</p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-stats-bars"></i>
+                </div>
+                <a href="/interest" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-lg-3 col-6">
+              <!-- small box -->
+              <div class="small-box bg-warning">
+                <div class="inner">
+                  <h3><?php echo e($invoices); ?></h3>
+
+                  <p>Invoices</p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-person-add"></i>
+                </div>
+                <a href="/invoice" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-lg-3 col-6">
+              <!-- small box -->
+              <div class="small-box bg-danger">
+                <div class="inner">
+                  <h3><?php echo e($customers); ?></h3>
+
+                  <p>Customers</p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-pie-graph"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
+            <!-- ./col -->
+          </div>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('css'); ?>
+    <link rel="stylesheet" href="/css/admin_custom.css">
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('js'); ?>
+    <script> console.log('Hi!'); </script>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('adminlte::page', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp_8.2\htdocs\ICBT\resources\views/home.blade.php ENDPATH**/ ?>
